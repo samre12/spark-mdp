11a098dcb8904d7a52cba8cff08c8faf3dba2d2b
Kartikay Garg <kartikay123@gmail.com>
2016-12-16 02:53:16 +0530

    add iterations to complete policy evaluation

--- src/main/java/spark_mdp/PolicyEvaluation.java
+++ src/main/java/spark_mdp/PolicyEvaluation.java
@@ -3,6 +3,7 @@ package spark_mdp;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.*;
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.mllib.linalg.distributed.*;
 import org.apache.spark.storage.StorageLevel;
 
@@ -12,57 +13,62 @@ public class PolicyEvaluation {
 	static private Double discount;
 	
 	static void main(String[] args) throws IllegalArgumentException{
-		JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("MDP"));
+		JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("Policy Evaluation"));
 		
 		num_states = Long.parseLong(args[0]);
 		num_actions = Long.parseLong(args[1]);
 		discount = Double.parseDouble(args[2]);
 		
+		final Broadcast<Long> num_states_BroadCast = sc.broadcast(num_states);
+		final Broadcast<Long> num_actions_BroadCast = sc.broadcast(num_actions);
+		final Broadcast<Double> discount_BroadCast = sc.broadcast(discount);
+		
 		checkAgruements(args);
-		BlockMatrix Pi = new CoordinateMatrix(sc.textFile(args[3]).map(new Function<String, MatrixEntry>() {
-			/**
-			 * 
-			 */
+		BlockMatrix Pi = new CoordinateMatrix(sc.textFile(args[4]).map(new Function<String, MatrixEntry>() {
 			private static final long serialVersionUID = 328024652664764883L;
 
 			@Override
-			public MatrixEntry call(String v1) throws Exception {
-				// TODO Auto-generated method stub
-				return null;
+			public MatrixEntry call(String s) throws Exception {
+				String[] parts = s.split(",");
+				Long state = Long.parseLong(parts[0]);
+				Long action = Long.parseLong(parts[1]);
+				return new MatrixEntry(state, (state - 1) * num_actions_BroadCast.value() + action, 1);
 			}
 			
 		}).rdd(), num_states, num_states * num_actions).toBlockMatrix().persist(StorageLevel.MEMORY_ONLY_SER());
 		
 		
-		BlockMatrix P = new CoordinateMatrix(sc.textFile(args[4]).map(new Function<String, MatrixEntry>() {
-			/**
-			 * 
-			 */
+		BlockMatrix P = new CoordinateMatrix(sc.textFile(args[5]).map(new Function<String, MatrixEntry>() {
 			private static final long serialVersionUID = 328024652664764883L;
 
 			@Override
-			public MatrixEntry call(String v1) throws Exception {
-				// TODO Auto-generated method stub
-				return null;
+			public MatrixEntry call(String s) throws Exception {
+				String[] parts = s.split(",");
+				Long state1 = Long.parseLong(parts[0]);
+				Long action = Long.parseLong(parts[1]);
+				Long state2 = Long.parseLong(parts[2]);
+				Double probability = Double.parseDouble(parts[3]);
+				return new MatrixEntry((state1 - 1) * num_actions_BroadCast.value() + action, state2, discount_BroadCast.value() * probability);
 			}
 		}).rdd(), num_states * num_actions, num_states).toBlockMatrix();
 		
 		BlockMatrix r = new CoordinateMatrix(sc.textFile(args[5]).map(new Function<String, MatrixEntry>() {
-			/**
-			 * 
-			 */
 			private static final long serialVersionUID = -5784388060110611464L;
 
 			@Override
-			public MatrixEntry call(String v1) throws Exception {
-				// TODO Auto-generated method stub
-				return null;
+			public MatrixEntry call(String s) throws Exception {
+				String[] parts = s.split(",");
+				Long state = Long.parseLong(parts[0]);
+				Long action = Long.parseLong(parts[1]);
+				Double r = Double.parseDouble(parts[2]);
+				return new MatrixEntry((state - 1) * num_actions_BroadCast.value() + action, 1, r);
 			}
 			
 		}).rdd(), num_states * num_actions, 1).toBlockMatrix();
 		
 		BlockMatrix H = Pi.multiply(P).persist(StorageLevel.MEMORY_AND_DISK_SER());
 		BlockMatrix vBar = Pi.multiply(r).persist(StorageLevel.MEMORY_AND_DISK_SER());
+		//vBar also serves as the initial approximation of v
 		
 		sc.stop();
 	}
